829da35030a62846f22c64ed28d7cf9f9495123f
LORD KAY <offeilord@gmail.com>
2018-01-29 20:24:13 +0000

    configuring response

--- console/uninstaller.sh
+++ console/uninstaller.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 HOSTNAME=`hostname`;
 HOSTIP=`hostname -I`;
+FULLDATA=`uname`;
+
+#TODO: Check if user wants to delete the library
+echo "Do you really wants to uninstall the configuration :-> (Y/y|N/n)";
+read RESPONSE;
+if [[ $RESPONSE -eq 'Y' ]] || [[ $RESPONSE -eq 'y' ]]; then
+    #TODO: Perform operation by uninstalling the app
+fi
