5feb4e2df8fbd1ca9f35eebf1bf53b30c3d9ce38
LORD KAY <offeilord@gmail.com>
2018-01-29 21:27:13 +0000

    now codes arrive

--- console/uninstaller.sh
+++ console/uninstaller.sh
@@ -3,24 +3,37 @@ HOSTNAME=`hostname`;
 HOSTIP=`hostname -I`;
 FULLDATA=`uname`;
 
-#TODO: Check if user wants to delete the library
-echo "Do you really wants to uninstall the configuration :-> (Y/y|N/n)";
-read RESPONSE;
-if [[ $RESPONSE -eq 'Y' ]] || [[ $RESPONSE -eq 'y' ]]; then
-    #TODO: Perform operation by uninstalling the app
-    echo "Uninstallation will take few seconds, Please sit back and relax ... ";
-    sleep 2
-    echo "Performing Removal of Scheduled Jobs ...";
-    crontab -i -r
-    if [[ $? -ne 1 ]]; then
-        #TODO: If executing is successful
-        echo "Scheduled Jobs Removed Completely ...";
-    else
-        #TODO: If executing fails
-        echo "Uninstalling Terminated ...";
-    fi
+###TODO: files configs
+SERVBASE="$PWD";
+BASEFILE="../basedirpath";
 
-elif [[ $RESPONSE -eq 'N' ]] || [[ $RESPONSE -eq 'n' ]]; then
-    #TODO: Exist uninstalling operation
-    exit;
-fi
+##TODO: Check if user wants to delete the library
+read -p  "Do you really wants to uninstall the configuration :-> (Y/y|N/n)" RESPONSE;
+case $RESPONSE in
+     [Yy]* )
+     #TODO;Perform operation by uninstalling the app
+     echo "Uninstallation will take few seconds, Please sit back and relax ...";
+     sleep 2
+     echo "Performing Removal of Scheduled Jobs ...";
+     #TODO: executing the job removal
+     crontab -r
+     sleep 2
+     echo "Removing Base Directory Path And Its Related Files ..."
+     if  [[ -f $BASEFILE ]]; then
+         #TODO: delete the file and its content --> PWD
+         sleep 3
+         rm $BASEFILE;
+         echo "Path Successfully Removed ..."
+     fi
+     ;;
+     [Nn]* )
+     #TODO:Exit uninstalling operation
+     exit;;
+     * )
+     echo "Incorrect Input Made ..";
+     sleep 1
+     echo "Program Terminating ...";
+     sleep 4
+     echo "Program Terminated";
+esac
+
